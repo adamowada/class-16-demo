bf68c42a69154b4abe079a770a0102df239d67ae
Adam Owada <adamowada@gmail.com>
2023-05-09 18:55:47 -0700

    finished hello.py function

--- api/hello.py
+++ api/hello.py
@@ -1,2 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 
+
+class handler(BaseHTTPRequestHandler):
+    # use this method name!
+    def do_GET(self):
+        message = "Hello"
+        self.send_response(200)  # HTTP code
+        self.send_header('Content-type', 'text/plain')  # define the content type
+        self.end_headers()  # add a blank line
+        self.wfile.write(message.encode())  # write the message
+        
